d0d860f379f7d4856b248e10ab023e8a076b0c31
developer <developer@MacBook-Pro-5.local>
2018-08-24 20:40:00 +1000

    .

--- app/src/main/java/com/example/developer/myapp/RemoteServerAPI.java
+++ app/src/main/java/com/example/developer/myapp/RemoteServerAPI.java
@@ -1,7 +1,46 @@
 package com.example.developer.myapp;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Dictionary;
+
+import javax.net.ssl.HttpsURLConnection;
+
 public class RemoteServerAPI {
 
-    
+    private void makeHTTPPOSTRequestFromURLWithParameterToValueMapping(String stringUrlToMakePOSTRequestTo, Dictionary<String, String> mappingOfParametersToValues) {
+        try {
+
+            URL urlToMakePOSTRequestTo = new URL(stringUrlToMakePOSTRequestTo);
+            HttpURLConnection connectionToURLForMakingPOSTRequest = (HttpURLConnection) urlToMakePOSTRequestTo.openConnection();
+            //conn.setReadTimeout(10000);
+            //conn.setConnectTimeout(15000);
+            connectionToURLForMakingPOSTRequest.setRequestMethod("POST");
+            //connectionToURLForMakingPOSTRequest.setDoInput(false);
+            //connectionToURLForMakingPOSTRequest.setDoOutput(false);
+
+            List<NameValuePair> params = new ArrayList<NameValuePair>();
+            params.add(new BasicNameValuePair("firstParam", paramValue1));
+            params.add(new BasicNameValuePair("secondParam", paramValue2));
+            params.add(new BasicNameValuePair("thirdParam", paramValue3));
+
+            OutputStream os = conn.getOutputStream();
+            BufferedWriter writer = new BufferedWriter(
+                    new OutputStreamWriter(os, "UTF-8"));
+            writer.write(getQuery(params));
+            writer.flush();
+            writer.close();
+            os.close();
+
+            conn.connect();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void sendMessageToRemoteServer(String messageToSend) {
+
+    }
 
 }
