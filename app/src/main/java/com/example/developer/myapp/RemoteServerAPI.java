52c58a4e8500a82ae3a081c2ad5d7426e851ca9f
developer <developer@MacBook-Pro-5.local>
2018-08-24 21:00:00 +1000

    .

--- app/src/main/java/com/example/developer/myapp/RemoteServerAPI.java
+++ app/src/main/java/com/example/developer/myapp/RemoteServerAPI.java
@@ -1,14 +1,50 @@
 package com.example.developer.myapp;
 
+import android.util.Pair;
+
+import java.io.BufferedWriter;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.net.ssl.HttpsURLConnection;
 
 public class RemoteServerAPI {
 
-    private void makeHTTPPOSTRequestFromURLWithParameterToValueMapping(String stringUrlToMakePOSTRequestTo, Dictionary<String, String> mappingOfParametersToValues) {
+
+    private static String generateHTTPParameterQueryStringFromMappingOfParametersToValues(Map<String, String> mappingOfParametersToValues) {
+        StringBuilder result = new StringBuilder();
+        boolean currentlyOnTheFirstParameterInTheList = true;
+
+        try {
+
+            for (Map.Entry<String, String> parameterToValueMapping : mappingOfParametersToValues.entrySet()) {
+                String parameter = parameterToValueMapping.getKey();
+                String value = parameterToValueMapping.getValue();
+
+                if (!currentlyOnTheFirstParameterInTheList) result.append("&");
+
+                result.append(URLEncoder.encode(parameter, "UTF-8"));
+                result.append("=");
+                result.append(URLEncoder.encode(value, "UTF-8"));
+
+                currentlyOnTheFirstParameterInTheList = false;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return result.toString();
+    }
+
+    private static void makeHTTPPOSTRequestFromURLWithParameterToValueMapping(String stringUrlToMakePOSTRequestTo, Map<String, String> mappingOfParametersToValues) {
         try {
 
             URL urlToMakePOSTRequestTo = new URL(stringUrlToMakePOSTRequestTo);
@@ -19,28 +55,34 @@ public class RemoteServerAPI {
             //connectionToURLForMakingPOSTRequest.setDoInput(false);
             //connectionToURLForMakingPOSTRequest.setDoOutput(false);
 
-            List<NameValuePair> params = new ArrayList<NameValuePair>();
-            params.add(new BasicNameValuePair("firstParam", paramValue1));
-            params.add(new BasicNameValuePair("secondParam", paramValue2));
-            params.add(new BasicNameValuePair("thirdParam", paramValue3));
+            OutputStream connectionOutputStream = connectionToURLForMakingPOSTRequest.getOutputStream();
+            BufferedWriter bufferedWriterForConnectionOutputStream = new BufferedWriter(
+                    new OutputStreamWriter(connectionOutputStream, "UTF-8"));
+            String parameterQueryString = generateHTTPParameterQueryStringFromMappingOfParametersToValues(mappingOfParametersToValues);
+            bufferedWriterForConnectionOutputStream.write(parameterQueryString);
+            bufferedWriterForConnectionOutputStream.flush();
+            bufferedWriterForConnectionOutputStream.close();
+            connectionOutputStream.close();
 
-            OutputStream os = conn.getOutputStream();
-            BufferedWriter writer = new BufferedWriter(
-                    new OutputStreamWriter(os, "UTF-8"));
-            writer.write(getQuery(params));
-            writer.flush();
-            writer.close();
-            os.close();
-
-            conn.connect();
+            connectionToURLForMakingPOSTRequest.connect();
 
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public static void sendMessageToRemoteServer(String messageToSend) {
 
+
+
+
+
+    private static final String REQUEST_URL_FOR_SENDING_MESSAGE_TO_SERVER = "http://127.0.0.1/send_message.php";
+    private static final String KEY_FOR_MESSAGE_PARAMETER = "message";
+
+    public static void sendMessageToRemoteServer(String messageToSend) {
+        HashMap parametersInRequest = new HashMap<String, String>();
+        parametersInRequest.put(KEY_FOR_MESSAGE_PARAMETER, messageToSend);
+        makeHTTPPOSTRequestFromURLWithParameterToValueMapping(REQUEST_URL_FOR_SENDING_MESSAGE_TO_SERVER, parametersInRequest);
     }
 
 }
--- app/src/main/java/com/example/developer/myapp/RemoteWebServerActivity.java
+++ app/src/main/java/com/example/developer/myapp/RemoteWebServerActivity.java
@@ -8,13 +8,33 @@
 
 package com.example.developer.myapp;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 public class RemoteWebServerActivity extends SRActivity {
+    private EditText inputMessageTextBox;
 
     private final int REMOTE_WEB_SERVER_UI_LAYOUT = R.layout.activity_remote_web_server;
     @Override
     protected void initialiseActivity() {
         this.setContentView(REMOTE_WEB_SERVER_UI_LAYOUT);
+        inputMessageTextBox = (EditText) this.findViewById(R.id.inputMessageTextBox);
+        this.initialiseSendMessageButton();
     }
 
+    private void initialiseSendMessageButton() {
+        Button sendMessageButton = (Button)this.findViewById(R.id.sendMessageButton);
+        sendMessageButton.setOnClickListener(this.whenSendMessageButtonIsPressedThenSendTheMessageFromTheTextBoxToRemoteServer()); // 2 things, when it does it (toggel button clicked) and what it does (toggel wifi)
+    }
+
+    private View.OnClickListener whenSendMessageButtonIsPressedThenSendTheMessageFromTheTextBoxToRemoteServer() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                RemoteWebServerActivity.this.toggelThisDevicesWifi();
+            }
+        };
+    }
 
 }
