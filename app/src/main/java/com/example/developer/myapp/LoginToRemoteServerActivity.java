a3a7479852e849b598be26cf79f1df43887c00d1
developer <developer@MacBook-Pro-5.local>
2018-08-28 21:20:00 +1000

    .

---
+++ app/src/main/java/com/example/developer/myapp/LoginToRemoteServerActivity.java
@@ -0,0 +1,23 @@
+//----------------------------------------------------------------------------------
+//
+// AUTHOR
+// ------
+// Lumberjacks Incorperated (2018)
+//
+//----------------------------------------------------------------------------------
+
+package com.example.developer.myapp;
+
+public class LoginToRemoteServerActivity extends SRActivity {
+
+    private final int LOGIN_SCREEN_UI_LAYOUT = R.layout.login_activity_screen;
+    @Override
+    protected void initialiseActivity() {
+        this.setContentView(LOGIN_SCREEN_UI_LAYOUT);
+        this.initialiseScreenButtons();
+    }
+
+    private void initialiseScreenButtons() {
+
+    }
+}
