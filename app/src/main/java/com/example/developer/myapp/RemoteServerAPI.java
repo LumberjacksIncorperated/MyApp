2d903ee72ba81695cb2461807ed716c7e3d49980
developer <developer@MacBook-Pro-5.local>
2018-08-29 11:52:16 +1000

    Broke down alot of functions

--- app/src/main/java/com/example/developer/myapp/RemoteServerAPI.java
+++ app/src/main/java/com/example/developer/myapp/RemoteServerAPI.java
@@ -8,63 +8,59 @@
 
 package com.example.developer.myapp;
 
-import android.os.Handler;
-import android.util.Pair;
-
-import java.io.BufferedWriter;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
+import java.io.IOException;
 import java.net.HttpURLConnection;
+import java.net.ProtocolException;
 import java.net.URL;
 import java.net.URLEncoder;
-import java.util.Dictionary;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-import javax.net.ssl.HttpsURLConnection;
-
 public class RemoteServerAPI {
-    
+
+    private static String createHTTPParameterStringSectionForParameterValueMapping(Map.Entry<String, String> parameterToValueMapping) {
+        String httpParameterString = "";
+        String parameter = parameterToValueMapping.getKey();
+        String value = parameterToValueMapping.getValue();
+        try {
+            httpParameterString +=  URLEncoder.encode(parameter, "UTF-8");
+            httpParameterString += "=";
+            httpParameterString += URLEncoder.encode(value, "UTF-8");
+        } catch (Exception thrownException) {
+
+        }
+        return httpParameterString;
+    }
+
     private static String generateHTTPParameterQueryStringFromMappingOfParametersToValues(Map<String, String> mappingOfParametersToValues) {
         String HTTPParameterQueryString = new String();
         boolean currentlyOnTheFirstParameterInTheList = true;
-        try {
-            for (Map.Entry<String, String> parameterToValueMapping : mappingOfParametersToValues.entrySet()) {
-                if (!currentlyOnTheFirstParameterInTheList) HTTPParameterQueryString += "&";
-                String parameter = parameterToValueMapping.getKey();
-                String value = parameterToValueMapping.getValue();
-                HTTPParameterQueryString +=  URLEncoder.encode(parameter, "UTF-8");
-                HTTPParameterQueryString += "=";
-                HTTPParameterQueryString += URLEncoder.encode(value, "UTF-8");
-                currentlyOnTheFirstParameterInTheList = false;
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+        for (Map.Entry<String, String> parameterToValueMapping : mappingOfParametersToValues.entrySet()) {
+            if (currentlyOnTheFirstParameterInTheList) HTTPParameterQueryString += "&";
+            HTTPParameterQueryString += createHTTPParameterStringSectionForParameterValueMapping(parameterToValueMapping);
+            currentlyOnTheFirstParameterInTheList = false;
         }
         return HTTPParameterQueryString;
     }
 
-    private static void setURLConenctionParametersForPOSTRequest(HttpURLConnection connectionToURLForMakingPOSTRequest) {
-        try {
+    private static void setURLConenctionParametersForPOSTRequest(HttpURLConnection connectionToURLForMakingPOSTRequest) throws ProtocolException {
             connectionToURLForMakingPOSTRequest.setReadTimeout(10000);
             connectionToURLForMakingPOSTRequest.setConnectTimeout(15000);
             connectionToURLForMakingPOSTRequest.setRequestMethod("GET");
             connectionToURLForMakingPOSTRequest.setDoInput(true);
             connectionToURLForMakingPOSTRequest.setDoOutput(true);
-        } catch (Exception thrownException) {
-            thrownException.printStackTrace();
-        }
     }
 
-    private static URL createURLToMakePOSTRequestToWithParameterQueryStringFromMappingAndStringURL(String stringUrlToMakePOSTRequestTo, Map<String, String> mappingOfParametersToValues) {
-        try {
-            String parameterQueryString = generateHTTPParameterQueryStringFromMappingOfParametersToValues(mappingOfParametersToValues);
-            URL urlToMakePOSTRequestTo = new URL(stringUrlToMakePOSTRequestTo + "?" + parameterQueryString);
-            return urlToMakePOSTRequestTo;
-        } catch (Exception thrownException) {
-            thrownException.printStackTrace();
-        }
+    public static URL createURLToMakePOSTRequestToWithParameterQueryStringFromMappingAndStringURL(String stringUrlToMakePOSTRequestTo, Map<String, String> mappingOfParametersToValues) throws IOException {
+        String parameterQueryString = generateHTTPParameterQueryStringFromMappingOfParametersToValues(mappingOfParametersToValues);
+        URL urlToMakePOSTRequestTo = new URL(stringUrlToMakePOSTRequestTo + "?" + parameterQueryString);
+        return urlToMakePOSTRequestTo;
+    }
+
+    private static void sendPOSTRequestWithUrlConenction( HttpURLConnection connectionToURLForMakingPOSTRequest) throws IOException {
+        connectionToURLForMakingPOSTRequest.connect();
+        connectionToURLForMakingPOSTRequest.getInputStream();
+        connectionToURLForMakingPOSTRequest.getResponseCode();
     }
 
     private static void makeHTTPPOSTRequestFromURLWithParameterToValueMapping(String stringUrlToMakePOSTRequestTo, Map<String, String> mappingOfParametersToValues) {
@@ -72,11 +68,7 @@ public class RemoteServerAPI {
             URL urlToMakePOSTRequestTo = createURLToMakePOSTRequestToWithParameterQueryStringFromMappingAndStringURL(stringUrlToMakePOSTRequestTo, mappingOfParametersToValues);
             HttpURLConnection connectionToURLForMakingPOSTRequest = (HttpURLConnection) urlToMakePOSTRequestTo.openConnection();
             setURLConenctionParametersForPOSTRequest(connectionToURLForMakingPOSTRequest);
-
-            connectionToURLForMakingPOSTRequest.connect();
-            connectionToURLForMakingPOSTRequest.getInputStream();
-            connectionToURLForMakingPOSTRequest.getResponseCode();
-
+            sendPOSTRequestWithUrlConenction(connectionToURLForMakingPOSTRequest);
         } catch (Exception thrownException) {
             thrownException.printStackTrace();
         }
