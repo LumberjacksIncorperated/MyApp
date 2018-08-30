c789883778afdf4512227d4a0c94939ba0d576ae
developer <developer@MacBook-Pro-5.local>
2018-08-30 21:20:00 +1000

    .

--- app/src/main/java/com/example/developer/myapp/LoginToRemoteServerActivity.java
+++ app/src/main/java/com/example/developer/myapp/LoginToRemoteServerActivity.java
@@ -8,16 +8,46 @@
 
 package com.example.developer.myapp;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 public class LoginToRemoteServerActivity extends BaseActivity {
+
+    private EditText usernameEditTextField;
+    private EditText passwordEditTextField;
+
 
     private final int LOGIN_SCREEN_UI_LAYOUT = R.layout.login_activity_screen;
     @Override
     protected void initialiseActivity() {
         this.setContentView(LOGIN_SCREEN_UI_LAYOUT);
-        this.initialiseScreenButtons();
+        this.initialiseUserInputFields();
+        this.initialiseLoginButton();
     }
 
-    private void initialiseScreenButtons() {
+    private void initialiseUserInputFields() {
+        this.usernameEditTextField = (EditText) this.findViewById(R.id.usernameEditText);
+        this.passwordEditTextField = (EditText) this.findViewById(R.id.passwordEditText);
+    }
 
+    private void initialiseLoginButton() {
+        Button loginButton = (Button)this.findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(this.whenLoginButtonIsPressedThenSendLoginRequestToRemoteServer());
+    }
+
+    private View.OnClickListener whenLoginButtonIsPressedThenSendLoginRequestToRemoteServer() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LoginToRemoteServerActivity.this.sendLoginRequestToRemoteServer();
+            }
+        };
+    }
+
+    private void sendLoginRequestToRemoteServer() {
+        String inputtedUsernameForLoginRequest = usernameEditTextField.getText().toString();
+        String inputtedPasswordForLoginRequest = passwordEditTextField.getText().toString();
+        RemoteServerAPI.sendMessageToRemoteServer(messageToSendToServer);
     }
 }
--- app/src/main/java/com/example/developer/myapp/RemoteServerAPI.java
+++ app/src/main/java/com/example/developer/myapp/RemoteServerAPI.java
@@ -18,6 +18,14 @@ import java.util.Map;
 
 public class RemoteServerAPI {
 
+    public interface RemoteServerAPIDelegate {
+        public void receiveResponseFromAPI(RemoteServerAPIResponse apiResponse);
+    }
+
+    public class RemoteServerAPIResponse {
+
+    }
+
     private static String createHTTPParameterStringSectionForParameterValueMapping(Map.Entry<String, String> parameterToValueMapping) {
         String httpParameterString = "";
         String parameter = parameterToValueMapping.getKey();
@@ -57,24 +65,28 @@ public class RemoteServerAPI {
         return urlToMakePOSTRequestTo;
     }
 
-    private static void sendPOSTRequestWithUrlConenction( HttpURLConnection connectionToURLForMakingPOSTRequest) throws IOException {
+    private static String sendPOSTRequestWithUrlConenctionAndReturnResponseString( HttpURLConnection connectionToURLForMakingPOSTRequest) throws IOException {
         connectionToURLForMakingPOSTRequest.connect();
         connectionToURLForMakingPOSTRequest.getInputStream();
         connectionToURLForMakingPOSTRequest.getResponseCode();
+        String responseString = connectionToURLForMakingPOSTRequest.getResponseMessage();
+        return responseString;
     }
 
-    private static void makeHTTPPOSTRequestFromURLWithParameterToValueMapping(String stringUrlToMakePOSTRequestTo, Map<String, String> mappingOfParametersToValues) {
+    private static void makeHTTPPOSTRequestFromURLWithParameterToValueMappingAndReturnResponseString(String stringUrlToMakePOSTRequestTo, Map<String, String> mappingOfParametersToValues) {
+        String responseString = null;
         try {
             URL urlToMakePOSTRequestTo = createURLToMakePOSTRequestToWithParameterQueryStringFromMappingAndStringURL(stringUrlToMakePOSTRequestTo, mappingOfParametersToValues);
             HttpURLConnection connectionToURLForMakingPOSTRequest = (HttpURLConnection) urlToMakePOSTRequestTo.openConnection();
             setURLConenctionParametersForPOSTRequest(connectionToURLForMakingPOSTRequest);
-            sendPOSTRequestWithUrlConenction(connectionToURLForMakingPOSTRequest);
+            responseString = sendPOSTRequestWithUrlConenctionAndReturnResponseString(connectionToURLForMakingPOSTRequest);
         } catch (Exception thrownException) {
             thrownException.printStackTrace();
         }
+        return responseString;
     }
 
-    private static void makeAsyncronousHTTPPOSTRequestFromURLWithParameterToValueMapping(final String stringUrlToMakePOSTRequestTo, final Map<String, String> mappingOfParametersToValues) {
+    private static void makeAsyncronousHTTPPOSTRequestFromURLWithParameterToValueMappingAndAPIDelegate(final String stringUrlToMakePOSTRequestTo, final Map<String, String> mappingOfParametersToValues, RemoteServerAPIDelegate apiDelegate) {
         Thread threadToSendMesasgeToServer = new Thread(new Runnable() {
             public void run() {
                 RemoteServerAPI.makeHTTPPOSTRequestFromURLWithParameterToValueMapping(stringUrlToMakePOSTRequestTo, mappingOfParametersToValues);
@@ -85,9 +97,9 @@ public class RemoteServerAPI {
 
     private static final String REQUEST_URL_FOR_SENDING_MESSAGE_TO_SERVER = "http://192.168.0.146/send_message.php";
     private static final String KEY_FOR_MESSAGE_PARAMETER = "message";
-    public static void sendMessageToRemoteServer(String messageToSend) {
+    public static void sendMessageToRemoteServerWithMessageToSendAndAPIDelegate(String messageToSend, RemoteServerAPIDelegate apiDelegate) {
         HashMap parametersInRequest = new HashMap<String, String>();
         parametersInRequest.put(KEY_FOR_MESSAGE_PARAMETER, messageToSend);
-        makeAsyncronousHTTPPOSTRequestFromURLWithParameterToValueMapping(REQUEST_URL_FOR_SENDING_MESSAGE_TO_SERVER, parametersInRequest);
+        makeAsyncronousHTTPPOSTRequestFromURLWithParameterToValueMappingAndAPIDelegate(REQUEST_URL_FOR_SENDING_MESSAGE_TO_SERVER, parametersInRequest, apiDelegate);
     }
 }
